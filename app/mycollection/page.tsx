61aeb887ed87ca18d2fdd2a24fb2518e6fae94bf
Anastasia Barsamian <abarsamian@mail.valenciacollege.edu>
2026-02-02 20:51:53 -0500

    styling

--- app/mycollection/page.tsx
+++ app/mycollection/page.tsx
@@ -13,7 +13,7 @@ export default function MyCollection() {
       </p>
 
       {/* List of Items */}
-      <ul className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {items.map((item) => (
               <div key={item.id} className="border border-gray-300 rounded-lg p-4 shadow-sm">
             <strong className="block text-lg">{item.name}</strong>
@@ -21,7 +21,7 @@ export default function MyCollection() {
           </div>
         ))}
         
-        </ul>
+        </div>
     </div>
 
 
