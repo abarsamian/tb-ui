920c023c798c2a886237d2531383f31fcb4421f4
Anastasia Barsamian <abarsamian@mail.valenciacollege.edu>
2026-02-09 13:13:18 -0500

    fixed mobile nav bar issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export function Navbar() {
   return (
 
-    <nav className="bg-black text-white p-4 flex items-center gap-6 justify-end">
+    <nav className="bg-black text-white p-4 flex flex-wrap items-center gap-6">
       
       <span className="font-extrabold text-4xl">ThriftBooks</span>
     
-      <div className="ml-auto flex items-center gap-6">
+      <div className="ml-auto flex flex-col sm:flex-row items-center gap-6">
       <Link href="/add-item" className="bg-gray-700 text-white px-4 py-2 rounded-md hover:bg-purple-700">Add Item</Link>
       <Link href="/" className="hover:text-purple-700">Home</Link>
       <Link href="/mycollection" className="hover:text-purple-700">My Collection</Link>
