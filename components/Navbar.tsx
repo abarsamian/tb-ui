9aad23938fc36313e7b8549d1d54bfb1ef00372a
Anastasia Barsamian <abarsamian@mail.valenciacollege.edu>
2026-02-16 14:18:14 -0500

    added logo home page function and moved add item button and deleted home button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,13 +5,12 @@ export function Navbar() {
 
     <nav className="bg-gradient-to-r from-violet-950 via-purple-950 to-gray-900 shadow-lg text-white p-4 flex flex-wrap items-center gap-6">
       
-      <span className="font-extrabold text-4xl">ThriftBooks</span>
+      <Link href="/"className="font-extrabold text-4xl">ThriftBooks</Link>
     
       <div className="ml-auto flex flex-col sm:flex-row items-end sm:items-center gap-4">
-      <Link href="/add-item" className="bg-gray-700 text-white px-4 py-2 rounded-md hover:bg-purple-700">Add Item</Link>
-      <Link href="/" className="hover:text-purple-700">Home</Link>
       <Link href="/mycollection" className="hover:text-purple-700">My Collection</Link>
       <Link href="/about" className="hover:text-purple-700">About</Link>
+      <Link href="/add-item" className="bg-gray-700 text-white px-4 py-2 rounded-md hover:bg-purple-700">Add Item</Link>
       </div>
     </nav>
 
