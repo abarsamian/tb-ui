f21f00edca32afb5f5fa90b0b6b80ec72ab6d7f3
Anastasia Barsamian <abarsamian@mail.valenciacollege.edu>
2026-02-18 21:55:06 -0500

    about us page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,8 @@
 export default function About() {
   return (
-    <div className="container mx-auto mt-10 p-4 justify-items-center">
-      <h1 className="text-3xl font-bold">About Us</h1>
-      <p className="mt-4 text-gray-600">
+    <div className="container mx-auto mt-10 p-4 justify-items-center space-y-10">
+      <h1 className="text-5xl font-bold">About Us</h1>
+      <p className="text-xl text-gray-600 mb-8 text-center">
         ThriftBooks is an easy and fun way to keep track of all your thrift finds. We know firsthand how much our vintage pieces mean to us, and now there is an easy way to keep track of all your most prized pieces!
       </p>
     </div>
